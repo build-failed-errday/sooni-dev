a572a0df24d3caae1f3663e75f60840977a431d1
build-failed-errday <heebum9588@naver.com>
2025-09-22 20:43:16 +0900

    layouting

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
+"use client"
+import { Textarea } from "@/components/ui/textarea"
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
-    <div className="flex justify-center items-center w-screen h-screen bg-radial from-gray-700  to-gray-900">
-      <div className="text-white"> sooni.dev </div>
+    <div className="flex flex-col justify-center items-center w-screen h-screen bg-radial from-gray-700  to-gray-900">
+      <div className="text-white "> sooni.dev </div>
+      <div className="">currently working on...</div>
+
+
+        <div className="grid w-160 gap-2">
+          <Textarea placeholder="What are you doing?" />
+          <Button variant="outline" onClick={()=>{}}>Send message</Button>
+        </div>
+
+
+      <div className="">I've done ...</div>
+      {/* 하스스톤 룰렛통 */}
     </div>
   );
 }
