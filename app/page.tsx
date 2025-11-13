1395e8b59155a4e88cd22742e07e91b8bd9a8de8
build-failed-errday <heebum9588@naver.com>
2025-11-13 22:40:59 +0900

    layouting

--- app/page.tsx
+++ app/page.tsx
@@ -5,33 +5,62 @@ import {
   // MainNavigationMenu,
   NavigationMenuDemo,
 } from "@/components/main-navigation-menu";
+import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { CardDemo } from "@/components/card-demo";
+import { CarouselDemo } from "@/components/carousel-demo";
 
 export default function Home() {
   return (
-    <div className="flex flex-col w-screen h-screen bg-radial from-neutral-700  to-neutral-800">
-      <div className="fixed-top w-full text-white flex justify-between items-center px-8">
-        <span className="text-2xl mr-4 py-2 font-bold cursor-pointer">
-          sooni.dev
-        </span>
-        <span className="">status here</span>
-      </div>
-      <div className="flex justify-center">
-        <NavigationMenuDemo />
-      </div>
-      <div className="text-white">currently working on...</div>
-      <div className="grid w-160 gap-2">
-        <Textarea placeholder="What are you doing?" />
-        <Button
-          className=""
-          variant="outline"
-          onClick={() => {}}
-        >
-          Send message
-        </Button>
-      </div>
+    <>
+      <AppSidebar />
+      <div className="flex flex-1 flex-col min-h-screen bg-radial from-neutral-700  to-neutral-800">
+        <header className="fixed-top w-full px-4 text-white flex justify-between items-center">
+          <SidebarTrigger />
+          <span className="text-2xl mr-4 py-2 font-bold cursor-pointer">
+            sooni.dev
+          </span>
+          <span className="">status here</span>
+        </header>
+        <main>
+          <section className=" p-8">
+            <div className="flex justify-center">
+              <CarouselDemo />
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+              <Card className="w-full max-w-sm">
+                <CardContent>
+                  <div className="text-white">currently working on...</div>
+                  <div className="grid gap-2">
+                    <Textarea placeholder="What are you doing?" />
+                    <Button
+                      className=""
+                      variant="outline"
+                      onClick={() => {}}
+                    >
+                      Send message
+                    </Button>
+                  </div>
+                  <div className="">I&apos;ve done ...</div>
+                </CardContent>
+              </Card>
+              <CardDemo />
+            </div>
 
-      <div className="">I&apos;ve done ...</div>
-      {/* 하스스톤 룰렛통 */}
-    </div>
+            {/*  */}
+            {/* 하스스톤 룰렛통 */}
+          </section>
+        </main>
+      </div>
+    </>
   );
 }
